9996905f212c45e40f6c16dcb0207ee11d89b386
Jolomi Ayu <jolomianthonyayu@gmail.com>
2026-04-08 08:55:47 +0000

    Protect tasks endpoint with JWT

--- src/create_task/app.py
+++ src/create_task/app.py
@@ -2,56 +2,69 @@ import json
 import logging
 import boto3
 import uuid
+import jwt
 
-# Logger setup
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# DynamoDB setup
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('TasksTable')
 
+SECRET_KEY = "mysecretkey"
+
 def lambda_handler(event, context):
     try:
-        # ✅ Log incoming request
         logger.info(f"Incoming event: {json.dumps(event)}")
 
-        # ✅ Simulate failure (for testing alarms later)
+        # ✅ AUTH CHECK
+        headers = event.get("headers", {})
+        auth_header = headers.get("Authorization")
+
+        if not auth_header:
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"message": "Missing token"})
+            }
+
+        token = auth_header.replace("Bearer ", "")
+
+        try:
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except Exception:
+            return {
+                "statusCode": 403,
+                "body": json.dumps({"message": "Invalid token"})
+            }
+
+        # ✅ Simulate failure
         if event.get("queryStringParameters") and event["queryStringParameters"].get("fail") == "true":
             raise Exception("Simulated failure for monitoring test")
 
-        # Parse body
         body = json.loads(event.get("body", "{}"))
         title = body.get("title", "No title")
 
-        # Create item
         task_id = str(uuid.uuid4())
         item = {
             "id": task_id,
             "title": title
         }
 
-        # Save to DynamoDB
         table.put_item(Item=item)
 
-        # ✅ Log success
         logger.info(f"Task created: {item}")
 
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": "Task created via CI/CD",
+                "message": "Task created (secured)",
                 "task": item
             })
         }
 
     except Exception as e:
-        # ✅ Log error
         logger.error(f"Error occurred: {str(e)}")
 
         return {
             "statusCode": 500,
-            "body": json.dumps({
-                "error": str(e)
-            })
+            "body": json.dumps({"error": str(e)})
         }
